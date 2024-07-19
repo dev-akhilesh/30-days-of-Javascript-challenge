4e1e26cc00414b0f1a85071838f9e1df50affa94
dev-akhilesh <akhileshtakawale703@gmail.com>
2024-07-19 14:15:31 +0530

    Day-7 challenge completed

---
+++ Day 07/featureRequest.js
@@ -0,0 +1,44 @@
+// Book Object Script: Creating a book object, adding methods, and logging properties and method results
+let book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    getTitleAndAuthor: function () {
+        return `${this.title} by ${this.author}`;
+    },
+    updateYear: function (newYear) {
+        this.year = newYear;
+    },
+    getTitleAndYear: function () {
+        return `${this.title} - (${this.year})`;
+    }
+};
+
+console.log(book);
+console.log(book.getTitleAndAuthor());
+book.updateYear(2023);
+console.log(book);
+console.log(book.getTitleAndYear());
+
+
+
+// Library Object Script: Defining a library object containing an array of book objects and logging the library's details
+let library = {
+    name: "City Library",
+    books: [
+        { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+        { title: "1984", author: "George Orwell", year: 1949 },
+        { title: "Moby Dick", author: "Herman Melville", year: 1851 }
+    ]
+};
+
+console.log(library);
+console.log("Library Name:", library.name);
+library.books.forEach(book => {
+    console.log("Book Title:", book.title);
+});
+
+
+
+
+
