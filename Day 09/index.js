4ececbd2727bb30570d3e6cbed30ccb4f600ff24
dev-akhilesh <akhileshtakawale703@gmail.com>
2024-07-21 22:01:23 +0530

    Day-9 challenge completed

---
+++ Day 09/index.js
@@ -0,0 +1,23 @@
+// Activity 1: Selecting and Manipulating Elements
+document.getElementById("myParagraph").textContent = "Changed Text";
+
+
+document.querySelector(".myClass").style.backgroundColor = "lightblue";
+
+
+
+// Activity 2: Creating and Appending Elements
+const newDiv = document.createElement('div');
+newDiv.textContent = 'Hello, I am a new div!';
+document.body.appendChild(newDiv);
+
+
+const newLi = document.createElement('li');
+newLi.textContent = 'Item 3';
+document.getElementById('myList').appendChild(newLi);
+
+
+
+// Activity 3: Removing Elements
+
+
