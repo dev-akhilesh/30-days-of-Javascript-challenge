7b2dd1d2e0fd30cfb8b6b25a24c7703d68a3c050
dev-akhilesh <akhileshtakawale703@gmail.com>
2024-07-22 23:07:59 +0530

    Day-10 challenge completed

--- Day 10/index.js
+++ Day 10/index.js
@@ -7,3 +7,25 @@ document.getElementById("changeTextBtn").addEventListener("click", function () {
 document.getElementById("toggleImage").addEventListener("dbclick", function () {
     this.style.display = this.style.display === "none" ? 'block' : 'none';
 });
+
+
+
+// Activity 2: Mouse Events
+document.getElementById("hoverElement").addEventListener("mouseover", function () {
+    this.style.backgroundColor = "lightblue";
+});
+
+
+document.getElementById('hoverElement').addEventListener('mouseout', function () {
+    this.style.backgroundColor = 'lightgray';
+});
+
+
+
+
+
+
+
+
+
+
