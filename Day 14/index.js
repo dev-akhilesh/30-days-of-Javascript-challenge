14d98074db53986248fb2c4d66e16e5aea48e28d
dev-akhilesh <akhileshtakawale703@gmail.com>
2024-07-26 22:44:30 +0530

    Day-14 challenge completed

--- Day 14/index.js
+++ Day 14/index.js
@@ -20,6 +20,26 @@ person1.updateAge(26);
 
 
 
+// Activity 2: Class Inheritance
+class Student extends Person {
+    constructor(name, age, studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+    getStudentId() {
+        return this.studentId;
+    }
+    greeting() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old, and my student ID is ${this.studentId}.`;
+    }
+}
+const student1 = new Student('Akhilesh', 24, 'S12345');
+console.log(student1.getStudentId()); // Output: S12345
+
+console.log(student1.greeting()); // Output: Hello, my name is Jane Doe, I am 20 years old, and my student ID is S12345.
+
+
+
 
 
 
